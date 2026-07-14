8294e38ccd185400a9086e7eca561cb57b8ced30
Santrina <f.klokov00@gmail.com>
2026-07-14 18:50:02 +0300

    first

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "/taxi";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
